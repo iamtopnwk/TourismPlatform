03a24b71c4ce2a353776d2cbb36bfa38528cdd14
Pabitra1 <pabitranou1@gmail.com>
2015-02-21 14:23:58 +0800

    I finished the IntroductionNote page ...

--- Tourismplatform/src/com/infotop/tourismplatform/introductionnote/IntroductionNoteActivity.java
+++ Tourismplatform/src/com/infotop/tourismplatform/introductionnote/IntroductionNoteActivity.java
@@ -1,22 +1,54 @@
 package com.infotop.tourismplatform.introductionnote;
 
+import java.util.concurrent.ExecutionException;
+
 import com.infotop.tourismplatform.R;
 import com.infotop.tourismplatform.R.id;
 import com.infotop.tourismplatform.R.layout;
 import com.infotop.tourismplatform.R.menu;
+import com.infotop.tourismplatform.introductionnote.model.IntroductionNote;
+import com.infotop.tourismplatform.urls.UrlInfo;
+import com.infotop.tourismplatform.utilities.GetOperation;
+import com.infotop.tourismplatform.utilities.JsonHelper;
 
 import android.support.v7.app.ActionBarActivity;
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class IntroductionNoteActivity extends Activity {
+	IntroductionNoteAdapter adapter;
+	ListView listView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_introduction_note);
+		
+		listView=(ListView)findViewById(R.id.introduction_list);
+		String serverUrl=UrlInfo.TAG_INTRODUCTION_NOTE;
+		
+		
+		AsyncTask<String, Void, String>introData=new GetOperation().execute(serverUrl);
+		try{
+			IntroductionNote[] iNotes=(IntroductionNote[]) JsonHelper.toObject(introData.get(), IntroductionNote[].class);
+	        adapter=new IntroductionNoteAdapter(IntroductionNoteActivity.this, iNotes);
+			listView.setAdapter(adapter);
+			
+			
+			
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 	@Override
---
+++ Tourismplatform/src/com/infotop/tourismplatform/urls/UrlInfo.java
@@ -0,0 +1,6 @@
+package com.infotop.tourismplatform.urls;
+
+public class UrlInfo {
+	public static String TAG_INTRODUCTION_NOTE="http://192.168.1.10:8080/TravelPlatform/rest/introductionNote";
+
+}
