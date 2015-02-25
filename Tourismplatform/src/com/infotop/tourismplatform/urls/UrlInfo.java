67600527a2b1f72c08e6aaf76c72afd3f5356657
Pabitra1 <pabitranou1@gmail.com>
2015-02-25 16:11:26 +0800

    Url changes

--- Tourismplatform/src/com/infotop/tourismplatform/urls/UrlInfo.java
+++ Tourismplatform/src/com/infotop/tourismplatform/urls/UrlInfo.java
@@ -1,10 +1,15 @@
 package com.infotop.tourismplatform.urls;
 
 public class UrlInfo {
-	public static String TAG_INTRODUCTION_NOTE="http://192.168.8.64:8080/TravelPlatform/rest/introductionNote";
-    public static String TAG_TRAFFIC="http://192.168.8.64:8080/TravelPlatform/rest/trafficModule";
-    public static String TAG_TRAVEL_NOTE="http://192.168.8.64:8080/TravelPlatform/rest/travelNote";
+	
+	public static String TAG_INTRODUCTION_NOTE="http://192.168.8.64:8080/TravelPlatform/rest/introductionNoteapp";
+    public static String TAG_TRAFFIC="http://192.168.8.64:8080/TravelPlatform/rest/trafficModuleapp";
+    public static String TAG_TRAVEL_NOTE="http://192.168.8.64:8080/TravelPlatform/rest/travelNoteapp";
     public static String ROOT_PATH="http://192.168.8.64:8080/TravelPlatform";
-    
+    public static String TAG_MALL_MODULE="http://192.168.8.64:8080/TravelPlatform/rest/mallModuleapp";
+    public static String TAG_BUY="http://192.168.8.64:8080/TravelPlatform/rest/specialBuyModuleapp";
+    public static String TAG_POPULAR_PLACE="http://192.168.8.64:8080/TravelPlatform/rest/popularPlaceModuleapp";
+    public static String TAG_LOCAL_TRANSPORT="http://192.168.8.64:8080/TravelPlatform/rest/localTransportModuleapp";
+
     
 }
